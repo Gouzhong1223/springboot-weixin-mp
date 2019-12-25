e27bc0fb116d0c01386043e94d9e6a72f069a911
Gouzhong <1162864960@qq.com>
2019-12-25 12:47:37 +0800

    新建工程

---
+++ src/main/java/com/gouzhong1223/springbootweixinmp/SpringbootWeixinMpApplication.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.springbootweixinmp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootWeixinMpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootWeixinMpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gouzhong1223/springbootweixinmp/SpringbootWeixinMpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouzhong1223.springbootweixinmp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootWeixinMpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
