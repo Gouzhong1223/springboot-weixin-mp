b4117ce9a1dd0342f9ce61f1f794a274485a58e9
Gouzhong <1162864960@qq.com>
2019-12-25 12:58:41 +0800

    新增微信请求接口

---
+++ src/main/java/com/gouzhong1223/springbootweixinmp/controller/WeixinMpController.java
@@ -0,0 +1,16 @@
+package com.gouzhong1223.springbootweixinmp.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author : Gouzhong
+ * @Blog : www.gouzhong1223.com
+ * @Description : 微信请求接口
+ * @Date : create by QingSong in 2019-12-25 12:57 下午
+ * @Email : 1162864960@qq.com
+ * @Since : JDK 1.8
+ * @ProjectName : springboot-weixin-mp
+ */
+@RestController
+public class WeixinMpController {
+}
